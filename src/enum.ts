ec848393a415661528c0f052f92b0a30bcb1d6a8
Ta-ryo <yarot.wsaiaik@outlook.jp>
2020-11-27 15:39:27 +0900

    learn about typeof union and literal and enum

---
+++ src/enum.ts
@@ -0,0 +1,46 @@
+export {};
+
+enum Months {
+  January = 1,
+  Feburary,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  Octorber,
+  November,
+  December,
+}
+
+console.log(Months.January);
+console.log(Months.Feburary);
+console.log(Months.December);
+
+// const MonthsJs = {
+//   Janualy: 0,
+//   Feburary: 1,
+// };
+
+// console.log(MonthsJs.Janualy);
+// console.log(MonthsJs.Feburary);
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFFF',
+  GREEN = '#008000',
+  BLUE = '#0000FF',
+  BLACK = '#000000',
+}
+
+let green = COLORS.GREEN;
+console.log(COLORS.GREEN);
+
+enum COLORS {
+  YELLO = '#FFFR00',
+  GRAY = '#808080',
+}
+
+COLORS.YELLO;
